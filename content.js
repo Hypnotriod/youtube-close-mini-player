112f3f7a0841803d5de3d8d5a152cb0ee226cbeb
illia <illia.pikin@gmail.com>
2026-02-08 12:24:41 +0200

    Refactoring

--- content.js
+++ content.js
@@ -1,20 +1,18 @@
 
-const waitForElement = (selector) =>
-    new Promise(resolve => {
+const observeElement = (selector) => new Promise(resolve => {
+    const element = document.querySelector(selector);
+    if (element) return resolve(element);
+    const observer = new MutationObserver(_ => {
         const element = document.querySelector(selector);
-        if (element) return resolve(element);
-        const observer = new MutationObserver(_ => {
-            const element = document.querySelector(selector);
-            if (element) {
-                observer.disconnect();
-                resolve(element);
-            }
-        });
-        observer.observe(document.body, { childList: true, subtree: true });
+        if (!element) return;
+        observer.disconnect();
+        resolve(element);
     });
+    observer.observe(document.body, { childList: true, subtree: true });
+});
 
 async function run() {
-    (await waitForElement('.ytp-miniplayer-close-button')).click();
+    (await observeElement('.ytp-miniplayer-close-button')).click();
     console.log(`The fucking "Continue Watching" YouTube mini-player is successfully closed!`);
 }
 
